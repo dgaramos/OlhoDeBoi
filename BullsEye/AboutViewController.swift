b8543437786a00711aeb3dddabbfea0938f8260c
Danilo Ramos <dgaramos@Mini-de-Danilo.home>
2018-09-29 20:26:58 -0300

    v1.0

--- BullsEye/AboutViewController.swift
+++ BullsEye/AboutViewController.swift
@@ -7,24 +7,31 @@
 //
 
 import UIKit
+import WebKit
 
 class AboutViewController: UIViewController {
 
+    @IBOutlet weak var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        webViewLoad(url: "https://github.com/dgaramos")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func displayGitHub() {
+        webViewLoad(url: "https://github.com/dgaramos")
     }
-    */
+    
+    func webViewLoad(url: String){
+        let url = URL(string:url)!
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+    
 
 }
--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -10,11 +10,82 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var currentValue: Int = 0
+    var targetValue: Int = 0
+    var score: Int = 0
+    var round: Int = 0
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var displayTargetValue: UILabel!
+    @IBOutlet weak var displayScore: UILabel!
+    @IBOutlet weak var displayRound: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let roundedValue = slider.value.rounded()
+        currentValue = Int(roundedValue)
+        startNewRound()
+        
+        let thumbImageNormal = UIImage(named: "BullsEye")!
+        slider.setThumbImage(thumbImageNormal, for: .normal)
+        let thumbImageSelected = UIImage(named: "BullsEyeSelected")!
+        slider.setThumbImage(thumbImageSelected, for: .highlighted)
     }
+    
+    @IBAction func showAlert() {
+        let difference: Int = abs(targetValue - currentValue)
+        let points: Int
+        
+        let title: String
+        
+        if difference == 0 {
+            title = "Brocador!"
+            points = 200
+        } else if difference < 5 {
+            title = "Quase hein?"
+            points = 150 - difference
+        } else if difference < 10 {
+            title = "Marromenos!"
+            points = 100 - difference
+        } else {
+            title = "Mermão!! Foi longe!"
+            points = 0
+        }
+        
+        score = score + points
+        
+        let message = "\nSua pontuação nessa rodada foi: \(points)"
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "Valeu meu patrão!", style: .default, handler: { action in self.startNewRound()})
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
 
-
+    }
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        let roundedValue = slider.value.rounded()
+        currentValue = Int(roundedValue)
+    }
+    
+    @IBAction func startOver() {
+        score = 0
+        round = 0
+        startNewRound()
+    }
+    
+    func startNewRound() {
+        round += 1
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateLabels()
+    }
+    
+    func updateLabels() {
+        displayTargetValue.text = String(targetValue)
+        displayScore.text = String(score)
+        displayRound.text = String(round)
+    }
 }
 
